9d578c66ca6b4aa6150832547aca4a423f637fed
Zach Parr <zachery.parr@gmail.com>
2020-09-14 19:48:31 -0500

    adding post call

--- routes/html.js
+++ routes/html.js
@@ -1,12 +1,16 @@
+// variables
 var router = require("express").Router()
 var path = require("path")
 
+// get home page
 router.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
+//get notes page
 router.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"))
 })
 
+// export for server.js
 module.exports = router
